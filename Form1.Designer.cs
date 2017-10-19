95d2772c8911f0e299cc82c3e9f2dadea50e4b97
Cosmin <Moisii_Ovidiu@yahoo.com>
2017-10-19 22:46:08 +0300

    adaugare databases

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.treeView1 = new System.Windows.Forms.TreeView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnAdaugare = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView1
@@ -38,25 +40,39 @@
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(162, 174);
             this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
-            // button1
+            // btnAdaugare
             // 
-            this.button1.Location = new System.Drawing.Point(53, 216);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAdaugare.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdaugare.Location = new System.Drawing.Point(44, 216);
+            this.btnAdaugare.Name = "btnAdaugare";
+            this.btnAdaugare.Size = new System.Drawing.Size(162, 23);
+            this.btnAdaugare.TabIndex = 1;
+            this.btnAdaugare.Text = "Adaugare";
+            this.btnAdaugare.UseVisualStyleBackColor = false;
+            this.btnAdaugare.Click += new System.EventHandler(this.btnAdaugare_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(236, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 227);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(704, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAdaugare);
             this.Controls.Add(this.treeView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -64,7 +80,8 @@
         #endregion
 
         private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAdaugare;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,60 @@ namespace BI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void btnAdaugare_Click(object sender, EventArgs e)
+        {
+            TreeNode animale = new TreeNode("animale");
+            
+            
+            TreeNode pisici = new TreeNode("pisici");
+            TreeNode pisiciNegre = new TreeNode("pisici negre");
+
+            pisici.Nodes.Add(pisiciNegre);
+            animale.Nodes.Add(pisici);
+
+            TreeNode pisiciNegreMici = new TreeNode("pisici negre mici");
+
+            pisiciNegre.Nodes.Add(pisiciNegreMici);
+
+            treeView1.Nodes.Add(animale);
+
+
+            String connection = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\Tables.accdb;Persist Security Info=True";
+            string sql = "SELECT Clients  FROM Tables";
+            using (OleDbConnection conn = new OleDbConnection(connection))
+            {
+                conn.Open();
+                DataSet ds = new DataSet();
+                DataGridView dataGridView1 = new DataGridView();
+                using (OleDbDataAdapter adapter = new OleDbDataAdapter(sql, conn))
+                {
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds;
+                    // Of course, before addint the datagrid to the hosting form you need to 
+                    // set position, location and other useful properties. 
+                    // Why don't you create the DataGrid with the designer and use that instance instead?
+                    this.Controls.Add(dataGridView1);
+                }
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
